d78f89d416053b7d5baa58a0a879c2bee33b4187
umit kumar ojha <60703019+umit123-tech@users.noreply.github.com>
2020-05-24 14:55:13 +0530

    switch case
    in this  switch case it has been shown how default value has been taken if the integer is not there.

---
+++ switch case.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i=5;
+	switch(i)
+	{
+		case 1:
+			printf("case 1");
+			break;
+	    case 2:
+	    	printf("case 2");
+	    	break;
+	    case 3:
+	    	printf("case 3");
+	    	break;
+	    case 4:
+	    	printf("case 4");
+	    	break;
+	    default:
+	    	printf("default");
+	}
+}
